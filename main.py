03b14b1bb5d915e97e7a6c5651b0400ee0c71746
Jackson <j@ckson.day>
2025-05-20 18:02:13 +1200

    Add Cell class, move code to Graphics class

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(50, 50), Point(200, 50)), "red")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Bot")
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-    
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-    
-    def close(self):
-        self.__running = False
-    
-    def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Line:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw(self, canvas, fill_color="black"):
-        canvas.create_line(
-            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
-        )
 
 main()
