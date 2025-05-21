2d575ad60e2ae2773220cc2d4543ee0d91356334
Jackson <j@ckson.day>
2025-05-21 19:34:25 +1200

    Add draw_line()

--- cell.py
+++ cell.py
@@ -26,3 +26,11 @@ class Cell:
             self.__window.draw_line(Line(Point(x1, y1), Point(x2, y1)), "red")
         if self.has_bottom_wall:
             self.__window.draw_line(Line(Point(x1, y2), Point(x2, y2)), "red")
+    
+    def draw_move(self, to_cell, undo = False):
+        colour = "gray" if undo else "red"
+        start_x = (self.__x1 + self.__x2) / 2
+        start_y = (self.__y1 + self.__y2) / 2
+        end_x = (to_cell.__x1 + to_cell.__x2) / 2
+        end_y = (to_cell.__y1 + to_cell.__y2) / 2
+        self.__window.draw_line(Line(Point(start_x, start_y), Point(end_x, end_y)), colour)
