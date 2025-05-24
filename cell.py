02873b72e4cb95148f9931beba1474a3137ba10e
Jackson <j@ckson.day>
2025-05-25 11:16:06 +1200

    Make entrance and exit

--- cell.py
+++ cell.py
@@ -1,8 +1,8 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, window):
-        self.__window = window
+    def __init__(self, window = None):
+        self.__win = window
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -13,19 +13,29 @@ class Cell:
         self.__y2 = -1
     
     def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
         self.__x1 = x1
         self.__y1 = y1
         self.__x2 = x2
         self.__y2 = y2
 
         if self.has_left_wall:
-            self.__window.draw_line(Line(Point(x1, y1), Point(x1, y2)), "red")
+            self.__win.draw_line(Line(Point(x1, y1), Point(x1, y2)), "red")
+        else:
+            self.__win.draw_line(Line(Point(x1, y1), Point(x1, y2)), "white")
         if self.has_right_wall:
-            self.__window.draw_line(Line(Point(x2, y1), Point(x2, y2)), "red")
+            self.__win.draw_line(Line(Point(x2, y1), Point(x2, y2)), "red")
+        else:
+            self.__win.draw_line(Line(Point(x2, y1), Point(x2, y2)), "white")
         if self.has_top_wall:
-            self.__window.draw_line(Line(Point(x1, y1), Point(x2, y1)), "red")
+            self.__win.draw_line(Line(Point(x1, y1), Point(x2, y1)), "red")
+        else:
+            self.__win.draw_line(Line(Point(x1, y1), Point(x2, y1)), "white")
         if self.has_bottom_wall:
-            self.__window.draw_line(Line(Point(x1, y2), Point(x2, y2)), "red")
+            self.__win.draw_line(Line(Point(x1, y2), Point(x2, y2)), "red")
+        else:
+            self.__win.draw_line(Line(Point(x1, y2), Point(x2, y2)), "white")
     
     def draw_move(self, to_cell, undo = False):
         colour = "gray" if undo else "red"
@@ -33,4 +43,4 @@ class Cell:
         start_y = (self.__y1 + self.__y2) / 2
         end_x = (to_cell.__x1 + to_cell.__x2) / 2
         end_y = (to_cell.__y1 + to_cell.__y2) / 2
-        self.__window.draw_line(Line(Point(start_x, start_y), Point(end_x, end_y)), colour)
+        self.__win.draw_line(Line(Point(start_x, start_y), Point(end_x, end_y)), colour)
--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win = None,
     ):
         self.x1 = x1
         self.y1 = y1
@@ -18,25 +18,37 @@ class Maze:
         self.num_cols = num_cols
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
-        self.win = win
+        self.__win = win
         self.__cells = []
 
         self.__create_cells()
+        self.__break_entrance_and_exit()
     
     def __create_cells(self):
         for i in range(self.num_rows):
             self.__cells.append([])
             for j in range(self.num_cols):
-                self.__cells[i].append(Cell(self.win))
+                self.__cells[i].append(Cell(self.__win))
                 self.__draw_cell(i, j)
     
     def __draw_cell(self, row, column):
-        cell_pos_x = self.x1 + ((row) * self.cell_size_x)
-        cell_pos_y = self.y1 + ((column) * self.cell_size_y)
+        if self.__win is None:
+            return
+        cell_pos_x = self.x1 + ((column) * self.cell_size_x)
+        cell_pos_y = self.y1 + ((row) * self.cell_size_y)
         self.__cells[row][column].draw(cell_pos_x, cell_pos_y, cell_pos_x + self.cell_size_x, cell_pos_y + self.cell_size_y)
         self._animate()
     
     def _animate(self):
-        self.win.redraw()
+        if self.__win is None:
+            return
+        self.__win.redraw()
         time.sleep(0.05)
+    
+    def __break_entrance_and_exit(self):
+        self.__cells[0][0].has_top_wall = False
+        self.__draw_cell(0, 0)
+        self.__cells[self.num_rows - 1][self.num_cols - 1].has_bottom_wall = False
+        self.__draw_cell(self.num_rows - 1, self.num_cols - 1)
+
  
